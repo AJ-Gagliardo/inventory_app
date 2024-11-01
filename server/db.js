e493a90f94d22e8a5fd78fdc812c7f6f7e4f41e2
Antonio Gagliardo <ajgagliardo92@gmail.com>
2024-11-01 11:27:16 -0700

    making small changes to be able to host it on railway

--- server/db.js
+++ server/db.js
@@ -7,12 +7,19 @@ const host = process.env.HOST;
 const port = process.env.PORT;
 const db = process.env.DATABASE;
 
+// this is for my local computer
+// const pool = new Pool({
+//   user: user,
+//   password: password,
+//   host: host,
+//   port: port,
+//   database: db,
+// });
+
+// this is with railway
 const pool = new Pool({
-  user: user,
-  password: password,
-  host: host,
-  port: port,
-  database: db,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = pool;
