8ce8cdbf2fff55acef354721b1f4aaeee4b8e7bb
Antonio Gagliardo <ajgagliardo92@gmail.com>
2024-11-01 11:40:16 -0700

    commenting out some db things that were only for local env

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 require("dotenv").config();
 
-const user = process.env.USER;
-const password = process.env.PASSWORD;
-const host = process.env.HOST;
-const port = process.env.PORT;
-const db = process.env.DATABASE;
+// const user = process.env.USER;
+// const password = process.env.PASSWORD;
+// const host = process.env.HOST;
+// const port = process.env.PORT;
+// const db = process.env.DATABASE;
 
 // this is for my local computer
 // const pool = new Pool({
